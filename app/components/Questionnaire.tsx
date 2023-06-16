6d7375999d3116853ea7e951ae84f89867101815
Vlad Simankov <mnrlmnstr@ml.local>
2023-06-16 13:10:58 +0300

    init

---
+++ app/components/Questionnaire.tsx
@@ -0,0 +1,86 @@
+'use client'
+
+import React, { useState } from "react";
+import questionsData from "../questions.json";
+
+
+interface Question {
+  question: string;
+  answers: { [key: string]: any };
+  [key: string]: any;
+}
+
+const Questionnaire: React.FC = () => {
+
+  const [currentQuestion, setCurrentQuestion] = useState<Question>(questionsData);
+  const [result, setResult] = useState<string>("");
+
+  const handleAnswer = (answer: Question | any) => {
+    if (answer) {
+      console.log("pushed answer: ", answer);
+      setCurrentQuestion(answer);
+    }
+
+    if (answer.result) {
+      setResult(answer.result);
+    }
+  };
+
+  const renderQuestions = () => {
+    if (currentQuestion.question) {
+      return (
+        <div>
+          <h2 className="text-xl font-bold">{currentQuestion.question}</h2>
+          <div className="flex mt-3 flex-row-reverse justify-end">
+            {Object.entries(currentQuestion.answers).map(([key, value]) => (
+              <button className="btn mr-1" key={key} onClick={() => handleAnswer(value)}>{key}</button>
+            ))}
+          </div>
+        </div>
+      );
+    } else {
+      const questionKeys = [
+        "question_1",
+        "question_2",
+        "question_3",
+        "question_4"
+      ];
+      let renderedQuestions: Question[] = [];
+
+      questionKeys.forEach((key) => {
+        if (currentQuestion[key]) {
+          renderedQuestions.push(currentQuestion[key]);
+        }
+      });
+
+      return (
+        <div>
+          {renderedQuestions.map((question, index) => (
+            <div className="mb-4" key={index}>
+              <h2 className="text-xl font-bold">{question.question}</h2>
+              <div className="flex mt-3 flex-row-reverse justify-end">
+                {Object.entries(question.answers).map(([key, value]) => (
+                  <button className="btn mr-1" key={key} onClick={() => handleAnswer(value)}>{key}</button>
+                ))}
+              </div>
+            </div>
+          ))}
+        </div>
+      );
+    }
+  };
+
+  return (
+    <div>
+      {renderQuestions()}
+      {result && (
+        <div>
+          <h2 className="text-xl font-bold">Result</h2>
+          <p>{result}</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Questionnaire;
